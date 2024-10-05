0f91ef0afda2613cac627ed9a414a3d6fe9c5c8b
Noyon141 <mofijurrahman2023@gmail.com>
2024-10-06 00:29:07 +0600

    Completed the initialization of project and made the theme dark using next themes & installed shadcn as well.

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import { ThemeProvider } from "@/components/theme-provider";
+import { Metadata } from "next";
+import { Roboto_Condensed } from "next/font/google";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "FlexBank",
+  description:
+    "FlexBank is an innovative web application designed to simplify and enhance your banking journey. Built on Next.js, it offers a user-friendly interface that allows you to effortlessly manage your finances from anywhere, at any time. With real-time insights into your accounts and transactions, you can make informed financial decisions with ease.",
+};
+
+const roboto = Roboto_Condensed({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${roboto.className} antialiased`}>
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
